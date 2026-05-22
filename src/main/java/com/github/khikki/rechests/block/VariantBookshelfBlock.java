8dd9636f8f8563c4ca953970a19758cd408ef7c0
Mark Purhin <mark.infinet@gmail.com>
2026-05-22 12:20:54 +0100

    Add bookshelf variant enum and block scaffold

--- src/main/java/com/github/khikki/rechests/block/VariantBookshelfBlock.java
+++ src/main/java/com/github/khikki/rechests/block/VariantBookshelfBlock.java
@@ -1,4 +1,18 @@
 package com.github.khikki.rechests.block;
 
-public class VariantBookshelfBlock {
+import com.github.khikki.rechests.bookshelf.BookshelfVariant;
+import net.minecraft.block.BlockBookshelf;
+
+public class VariantBookshelfBlock extends BlockBookshelf {
+    private final BookshelfVariant variant;
+
+    public VariantBookshelfBlock(BookshelfVariant variant) {
+        this.variant = variant;
+        setBlockName(variant.getBlockName());
+        setHardness(1.5F);
+        setStepSound(soundTypeWood);
+    }
+    public BookshelfVariant getBookshelfVariant() {
+        return variant;
+    }
 }
--- src/main/java/com/github/khikki/rechests/bookshelf/BookshelfVariant.java
+++ src/main/java/com/github/khikki/rechests/bookshelf/BookshelfVariant.java
@@ -1,19 +1,21 @@
 package com.github.khikki.rechests.bookshelf;
 
 public enum BookshelfVariant {
-    OAK("oak", "oakBookshelf"),
-    SPRUCE("spruce", "spruceBookshelf"),
-    BIRCH("birch", "brichBookshelf"),
-    JUNGLE("jungle", "jungleBookshelf"),
-    ACACIA("acacia", "acaciaBookshelf"),
-    DarkOAK("dark_oak", "darkOakBookshelf");
+    OAK("oak", "oakBookshelf", "tile.oakBookshelf.name"),
+    SPRUCE("spruce", "spruceBookshelf", "tile.spruceBookshelf.name"),
+    BIRCH("birch", "birchBookshelf", "tile.birchBookshelf.name"),
+    JUNGLE("jungle", "jungleBookshelf", "tile.jungleBookshelf.name"),
+    ACACIA("acacia", "acaciaBookshelf", "tile.acaciaBookshelf.name"),
+    DARK_OAK("dark_oak", "darkOakBookshelf", "tile.darkOakBookshelf.name");
 
     private final String id;
     private final String blockName;
+    private final String inventoryName;
 
-    private BookshelfVariant(String id, String blockName) {
+    private BookshelfVariant(String id, String blockName, String inventoryName) {
         this.id = id;
         this.blockName = blockName;
+        this.inventoryName = inventoryName;
     }
 
     public String getId() {
@@ -22,5 +24,8 @@ public enum BookshelfVariant {
 
     public String getBlockName() {
         return blockName;
+    }
+    public String getInventoryName() {
+        return inventoryName;
     }
 }
